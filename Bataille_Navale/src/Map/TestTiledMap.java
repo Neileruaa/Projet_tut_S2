645369b7a4988b8b049f4e52c643aead3273b385
Aurel <aureliendrey@gmail.com>
2018-03-28 20:39:53 +0200

    Basique window vide

--- Bataille_Navale/src/Map/TestTiledMap.java
+++ Bataille_Navale/src/Map/TestTiledMap.java
@@ -1,5 +1,45 @@
 package Map;
 
-public class TestTiledMap {
+import org.newdawn.slick.*;
+import org.newdawn.slick.Graphics;
 
+import java.awt.*;
+
+public class TestTiledMap extends BasicGame {
+    private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+    public TestTiledMap()
+    {
+        super("Test de map");
+    }
+
+    public static void main(String[] arguments) {
+        try     {
+            AppGameContainer app = new AppGameContainer(new TestTiledMap());
+            //app.setDisplayMode(screenSize.width, screenSize.height, true); //=> Full screen
+            app.setDisplayMode(1600 , 900, false);
+            app.setShowFPS(true); // true for display the numbers of FPS
+            app.setVSync(true); // false for disable the FPS synchronize
+            app.start();
+
+        }   catch (SlickException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
 }
