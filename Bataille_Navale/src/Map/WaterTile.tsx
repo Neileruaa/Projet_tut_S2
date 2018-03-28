ba0a73885d56373079916a6eef3d70ca2f7b4b60
Aurel <aureliendrey@gmail.com>
2018-03-28 20:41:12 +0200

    Import de la carte tmx et d'une tile Water tsx

---
+++ Bataille_Navale/src/Map/WaterTile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Water" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../../../../Téléchargements/1445178173Water-C-16_-325x325.png" width="325" height="325"/>
+</tileset>
