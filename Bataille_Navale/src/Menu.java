7a80a13d6e3c3d4956a5beb692262914bffd2b07
Hugo <hugo90jak@gmail.com>
2018-03-29 18:51:46 +0200

    menu boutton

--- Bataille_Navale/src/Menu.java
+++ Bataille_Navale/src/Menu.java
@@ -18,11 +18,27 @@ public class Menu extends BasicGameState{
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException { //vous pouvez pour vous rappelez de ce qu'est render, en vous disant que render=afficher
         graphics.drawImage(imgMenu, 0, 0);
+
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+        int posX = Mouse.getX();
+        int posY = Mouse.getY();
 
+        //JOUER
+        if((posX>600 && posX<838) && (posY>232 && posY<332) ){
+            if (Mouse.isButtonDown(0)){
+                System.exit(0);
+            }
+        }
+
+        //QUITTER
+        if((posX>600 && posX<838) && (posY>600 && posY<700) ){
+            if (Mouse.isButtonDown(0)){
+                stateBasedGame.enterState(3);
+            }
+        }
     }
 
     @Override
