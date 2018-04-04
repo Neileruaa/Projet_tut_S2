45a6adb1b71780f8c9d0ee2265b7475ef7ee8005
Léa <chanel29_msn@hotmail.fr>
2018-04-04 16:09:15 +0200

    img de Léa

--- Bataille_Navale/out/production/Bataille_Navale/Map/tileWaterBorder.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="TileWaterBorder" tilewidth="90" tileheight="90" tilecount="1" columns="1">
- <image source="../Images/tile#1.png" width="90" height="90"/>
-</tileset>
