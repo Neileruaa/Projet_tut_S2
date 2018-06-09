4ada89802c2d1498c6ce0bf4ff5cfd428b8f687f
Matteo Billet <matteo25310@gmail.com>
2018-06-09 17:03:38 +0100

    ajout image+touche coule detection

--- Bataille_Navale/src/ScreenShoot.java
+++ Bataille_Navale/src/ScreenShoot.java
@@ -1,3 +1,4 @@
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.state.BasicGameState;
@@ -17,6 +18,25 @@ public class ScreenShoot extends BasicGameState {
 
     SpriteSheet waterSheet;
     Animation waterAnimation;
+
+    // image
+    private Image tire;
+    private Image passe;
+
+    // popup
+    private Image dialogueTouche;
+    private Image couleCorvette;
+    private Image couleCroiseur;
+    private Image couleSousMarin;
+    private Image couleCuirasse;
+    private Image coulePorteAvion;
+    private Image dialogueVictoire;
+    private Image rate;
+
+    private int[] space = {90,180,270,360,450,540,630,720,810,900};
+
+    private boolean dejaTire=false;
+    private String choix="";
 
     public ScreenShoot(int state){
     }
@@ -38,13 +58,81 @@ public class ScreenShoot extends BasicGameState {
         explosionAnimation = new Animation(explosionSheet, 42);
         waterSheet = new SpriteSheet("res/Images/water.png", 90,90);
         waterAnimation = new Animation(waterSheet, 100);
+
+        //image bouton
+        tire=new Image("res/Images/tire.png");
+        passe=new Image("res/Images/passe.png");
+
+        // popup de dialogue du commandant
+        couleCorvette=new Image("res/Images/couleCorvette.png");
+        couleCroiseur=new Image("res/Images/couleCroiseur.png");
+        couleSousMarin=new Image("res/Images/couleSousmarin.png");
+        couleCuirasse=new Image("res/Images/couleCuirasse.png");
+        coulePorteAvion=new Image("res/Images/coulePorteAvion.png");
+        dialogueVictoire=new Image("res/Images/dialogueVictoire.png");
+        dialogueTouche=new Image("res/Images/dialogueTouche.png");
+        rate=new Image("res/Images/rate.png");
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
 //        map.render(0,0,0,0,900,900);
         afficherMap(graphics);
+        graphics.drawImage(tire,900,400);
+        graphics.drawImage(passe,900,810);
+
+        Input input = gameContainer.getInput(); // pour le clic
+        int posX= Mouse.getX();
+        int posY=900- Mouse.getY();
+        int[] caseSup = findIdTile(posX, posY);
+        if((posX>caseSup[0]-90 && posX<caseSup[0])
+                && (posY>caseSup[1]-90 && posY<caseSup[1]) ){
+            graphics.setColor(new Color(255,0,255,0.5f));
+            graphics.fillRect(caseSup[0]-90, caseSup[1]-90, 90, 90);
+        }
+
+        choixDeTire(Mouse.getX(),900-Mouse.getY());
+
+        if(Mouse.isButtonDown(0) && !Mouse.isButtonDown(1)) { // clic gauche
+            if ((posX>0 && posX<900) && (posY>0 && posY<900) && !dejaTire){ // si il clic dans la grille
+                // et qu'il n'a pas deja tire
+                System.out.println("clic dans grille");
+                System.out.println(choix);
+                if(choix.equals("tire normal")){
+                    // tire matrice avec en parametre posX et posY
+                    System.out.println("vous effectuez un tire normal à la position X : "+posX+", Y : "+posY);
+                    dejaTire=true;
+                }
+            }
+            System.out.println("X : "+posX+", Y : "+posY);
+        }
+
         afficherAnimation();
+    }
+
+    public void choixDeTire(int posX, int posY){
+        if ((posX>890 && posX<1041) && (posY>390 && posY<492)){
+            if (Mouse.isButtonDown(0)){
+                System.out.println("vous avez choisit le tire normal");
+                choix="tire normal";
+            }
+        }
+    }
+
+    public int[] findIdTile(int posX, int posY){
+        int[] coord= new int[2];
+        for (int i = 0; i<space.length;i++){
+            if (posX < space[i]){
+                for (int j =0; j<space.length;j++){
+                    if (posY < space[j]){
+                        coord[0]=space[i];
+                        coord[1]=space[j];
+                        return coord;
+                    }
+                }
+            }
+        }
+        return coord; //retourne un tableau avec x et y correspondant au point en haut a gauche de la case ou on clic
     }
 
     private void afficherAnimation() {
@@ -103,5 +191,20 @@ public class ScreenShoot extends BasicGameState {
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
             waterAnimation.update(100);
+            passerTour(stateBasedGame);
+    }
+
+    public void passerTour(StateBasedGame stateBasedGame){
+        int posX = Mouse.getX();
+        int posY = Mouse.getY();
+        if(dejaTire && (posX>900 && posX<1170) && (posY>0 && posY<100) ){
+            if (Mouse.isButtonDown(0)){ // on sauvegarde les changements et on passe à l'autre joueur
+
+                System.out.println("on sauvegarde les changements et on passe à l'autre joueur");
+                //saverReader.saveEcranTir(1, plateau);
+                //saverReader.initEcranTir(1);
+                //stateBasedGame.enterState(2);
+            }
+        }
     }
 }
