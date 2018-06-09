fb42e39d5db3eb36a08a7b21692576374a271cc2
Matteo Billet <matteo25310@gmail.com>
2018-06-09 18:59:25 +0100

    correction bogue musique

--- Bataille_Navale/src/Menu.java
+++ Bataille_Navale/src/Menu.java
@@ -22,8 +22,8 @@ public class Menu extends BasicGameState{
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
 
         imgMenu=new Image("res/Images/imgMenu.png"); // Affiche l'image
-        survolBouton=new Sound("res/Musique/survolBouton.wav"); // Affecte un nouveau son à la variable
-        musiqueJeu=new Music("res/Musique/lancementMissile.wav"); // Affecte une nouvelle musique à la variable
+        survolBouton=new Sound("res/Musique/survolBouton1.wav"); // Affecte un nouveau son à la variable
+        musiqueJeu=new Music("res/Musique/lancementMissile1.wav"); // Affecte une nouvelle musique à la variable
         //musiqueJeu.loop(); // Boucle la musique
         nbFoisSurBoutonJouer=0; // Initialise le compteur à 0
         nbFoisSurBoutonQuitter=0; // Initialise le compteur à 0
