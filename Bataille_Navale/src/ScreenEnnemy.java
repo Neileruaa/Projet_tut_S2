4d010212e77eda2058fd179901e6053f7b328cfc
ChaliSky <36888688+ChaliSky@users.noreply.github.com>
2018-06-10 21:42:06 +0200

    Update ScreenEnnemy.java

--- Bataille_Navale/src/ScreenEnnemy.java
+++ Bataille_Navale/src/ScreenEnnemy.java
@@ -22,9 +22,9 @@ public class ScreenEnnemy extends ScreenPlayer{
         timer-=i;
         if(isBateauxPoses() && (posX>1170 && posX<1440) && (posY>0 && posY<100) || timer<= 0){
             if (Mouse.isButtonDown(0) && timer > 0){
-                saverReader.savePlateau(1, plateau);
-                saverReader.initEcranTir(1);
-                stateBasedGame.enterState(2);
+                saverReader.savePlateau(2, plateau);
+                saverReader.initEcranTir(2);
+                stateBasedGame.enterState(5);
             }else if(timer<=0){
                 stateBasedGame.enterState(4);
             }
